8e4f017662ac3b7a9977d0cf4b8aab61af68f7e7
urukov <urukov@keysystems.ru>
2021-06-15 00:34:11 +0300

    расширена карта

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from time import sleep
 from simulation import CityModel
 
 roads = [
-    RoadPart(point_1=Point(2.5, 0.0), point_2=Point(17.5, 0.0), auto_create_for_direction=False),
+    RoadPart(point_1=Point(2.5, 0.0), point_2=Point(17.5, 0.0)),
     RoadPart(point_1=Point(-2.5, 0.0), point_2=Point(-17.5, 0.0), auto_create_for_direction=False),
     RoadPart(
         point_1=Point(x=0.0, y=-2.5),
@@ -28,27 +28,39 @@ roads = [
 ]
 
 line1, line2, line3, line4 = gen_lines_around(Point(0.0, 20.0))
-roads.append(RoadPart(point_1=line1.p1, point_2=line1.p2, auto_create_for_direction=False))
-roads.append(RoadPart(point_1=line2.p1, point_2=line2.p2, auto_create_for_direction=False))
-roads.append(RoadPart(point_1=line4.p1, point_2=line4.p2, auto_create_for_direction=False, rotation_angle=math.pi/2))
+roads.append(RoadPart(point_1=line1.p1, point_2=line1.p2))  # 4
+roads.append(RoadPart(point_1=line2.p1, point_2=line2.p2, auto_create_for_direction=False))  # 5
+roads.append(RoadPart(point_1=line4.p1, point_2=line4.p2, auto_create_for_direction=False, rotation_angle=math.pi/2))  # 6
 
 line1, line2, line3, line4 = gen_lines_around(Point(0.0, -20.0))
 
-roads.append(RoadPart(point_1=line1.p1, point_2=line1.p2, auto_create_for_direction=False))
-roads.append(RoadPart(point_1=line2.p1, point_2=line2.p2, auto_create_for_direction=False))
-roads.append(RoadPart(point_1=line3.p1, point_2=line3.p2, auto_create_for_direction=False, rotation_angle=math.pi/2))
+roads.append(RoadPart(point_1=line1.p1, point_2=line1.p2, auto_create_for_direction=False))  # 7
+roads.append(RoadPart(point_1=line2.p1, point_2=line2.p2, auto_create_for_direction=False))  # 8
+roads.append(RoadPart(point_1=line3.p1, point_2=line3.p2, auto_create_for_direction=False, rotation_angle=math.pi/2))  # 9
 
 line1, line2, line3, line4 = gen_lines_around(Point(20.0, 0.0))
 
-roads.append(RoadPart(point_1=line1.p1, point_2=line1.p2, auto_create_for_direction=False))
-roads.append(RoadPart(point_1=line3.p1, point_2=line3.p2, rotation_angle=math.pi/2, auto_create_for_direction=False))
-roads.append(RoadPart(point_1=line4.p1, point_2=line4.p2, rotation_angle=math.pi/2, auto_create_for_direction=False))
+roads.append(RoadPart(point_1=line1.p1, point_2=line1.p2, auto_create_for_direction=False))  # 10
+roads.append(RoadPart(point_1=line3.p1, point_2=line3.p2, rotation_angle=math.pi/2))  # 11
+roads.append(RoadPart(point_1=line4.p1, point_2=line4.p2, rotation_angle=math.pi/2))  # 12
+
+line1, line2, line3, line4 = gen_lines_around(Point(20.0, 20.0))
+
+roads.append(RoadPart(point_1=line1.p1, point_2=line1.p2, auto_create_for_direction=False))  # 13
+roads.append(RoadPart(point_1=line4.p1, point_2=line4.p2, auto_create_for_direction=False, rotation_angle=math.pi/2))  # 14
+
+for i in range(len(roads)):
+    road = roads[i]
+    if road.line.p1.x in [17.5, 2.5] and road.line.p2.x in [2.5, 17.5] and road.line.p1.y in [20.0]:
+        print(i)
+
 
 cross_roads = [
     CrossRoad(position=Point(0.0, 0.0), roads=[roads[0], roads[1], roads[2], roads[3]]),
     CrossRoad(position=Point(0.0, 20.0), roads=[roads[3], roads[4], roads[5], roads[6]]),
     CrossRoad(position=Point(0.0, -20.0), roads=[roads[2], roads[7], roads[8], roads[9]]),
     CrossRoad(position=Point(20.0, 0.0), roads=[roads[0], roads[10], roads[11], roads[12]]),
+    CrossRoad(position=Point(20.0, 20.0), roads=[roads[13], roads[4], roads[12], roads[14]]),
 ]
 
 simulation_model = CityModel()
