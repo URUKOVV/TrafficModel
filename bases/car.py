93fe39f44ddbb55e6640291e60b7a0ed2001971f
urukov <urukov@keysystems.ru>
2021-06-16 22:11:57 +0300

    имитация задержки для автомобилей

--- bases/car.py
+++ bases/car.py
@@ -5,10 +5,14 @@ from .primitives import SimulateMixin, Point
 
 DEFAULT_CAR_LENGTH = 1
 DEFAULT_CAR_SPEED = 1.5
+DEFAULT_CAR_ACCELERATION_SPEED = 1.0
+DEFAULT_CAR_DELAY = 0.25
 
 
 class Car(SimulateMixin):
     CAR_COUNT = 0
+    STATE_STOPPED = 1
+    STATE_RUNNING = 2
     average_wait_time: float
     position: Point
     length = DEFAULT_CAR_LENGTH  # стандартная длина авто
@@ -20,6 +24,9 @@ class Car(SimulateMixin):
         self.drive_line = drive_line
         self.next_drive_line = None
         self.id = Car.inc_car_count()
+        self.speed = 0.0
+        self.state = self.STATE_STOPPED
+        self.delay_passed = 0.0
 
     @classmethod
     def inc_car_count(cls):
@@ -40,6 +47,16 @@ class Car(SimulateMixin):
                 (self.drive_line.line.p1.x, self.drive_line.line.p1.y),
                 (next_car.position.x, next_car.position.y)
             )
+            if next_car.state == Car.STATE_STOPPED and math.dist((self.position.x, self.position.y), (next_car.position.x, next_car.position.y)) == DEFAULT_CAR_LENGTH + 0.5:
+                return moved
+        if self.state == self.STATE_STOPPED:
+            self.delay_passed += timedelta
+            if self.delay_passed >= DEFAULT_CAR_DELAY:
+                self.delay_passed = 0.0
+                self.state = self.STATE_RUNNING
+            else:
+                return moved
+
         line_start_point = self.drive_line.line.p1
         distance_p1 = math.dist(
             (self.position.x, self.position.y), (line_start_point.x, line_start_point.y)
@@ -48,8 +65,8 @@ class Car(SimulateMixin):
             # проверка на то, что авто находится внутри полосы движения (между точки p1 и p2)
             assert distance_p1 < drive_line_distance
             new_position = Point(
-                self.position.x + self.drive_line.line_vector.x * timedelta * DEFAULT_CAR_SPEED,
-                self.position.y + self.drive_line.line_vector.y * timedelta * DEFAULT_CAR_SPEED
+                self.position.x + self.drive_line.line_vector.x * timedelta * self.speed,
+                self.position.y + self.drive_line.line_vector.y * timedelta * self.speed
             )
             distance_new_pos = math.dist((new_position.x, new_position.y), (line_start_point.x, line_start_point.y))
             # новая точка все еще находится внутри полосы
@@ -77,7 +94,13 @@ class Car(SimulateMixin):
             self.position = new_position
             moved = True
         except AssertionError:
+            self.state = self.STATE_STOPPED
+            self.speed = 0.0
             return moved
+        if self.speed < DEFAULT_CAR_SPEED:
+            self.speed += timedelta * DEFAULT_CAR_ACCELERATION_SPEED
+            if self.speed > DEFAULT_CAR_SPEED:
+                self.speed = DEFAULT_CAR_SPEED
         return moved
 
     def to_dict(self):
